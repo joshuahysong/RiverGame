b56b84aa1430ad9b311362314e9d0b1e3b960688
Joshua Hysong <joshuahysong@gmail.com>
2024-09-16 10:40:53 -0400

    Fix sorting and added tie breaker

--- src/components/GameEnd.vue
+++ src/components/GameEnd.vue
@@ -5,14 +5,14 @@
         <div class="card-body pt-0 pb-2 px-2">
             <div class="row no-gutters align-items-center pb-2">
                 <div class="col">
-                    {{ getScores()[0].player.name }} is the Winner!
+                    {{ getWinnerName(winningPlayerId) }} is the Winner!
                 </div>
             </div>
-            <div v-for="(playerScore, index) in getScores()"
+            <div v-for="(playerScore, index) in playerScores"
                 :key="index"
                 class="row no-gutters align-items-center justify-content-center small">
                 <div class="col-auto">
-                    #{{ index + 1 }}: {{ playerScore.player.name }} ({{ playerScore.finalScore }} points)
+                    #{{ index + 1 }}: {{ playerScore.player.name }} ({{ playerScore.score[0] }} points)
                 </div>
             </div>
         </div>
@@ -26,11 +26,19 @@ export default {
     name: 'GameEnd',
     data() {
         return {
-            confettiInstances: []
+            confettiInstances: [],
+            playerScores: [],
+            winningPlayerId: 0
         }
     },
     mounted() {
-        this.confettiInstances = [...Array(150).keys()]
+        this.confettiInstances = [...Array(150).keys()],
+        this.playerScores =  this.getScores()
+        this.winningPlayerId = this.playerScores[0].player.id
+        this.$store.commit('log/logActionMessage', {
+            playerId: this.winningPlayerId,
+            text: `wins the game with 0 points`
+        })
     },
     computed: {
         ...mapGetters('game', [
@@ -41,60 +49,75 @@ export default {
         }),
     },
     methods: {
-      getScores() {
-        let playerScores = []
-        for (const player of this.allPlayers) {
-            let treasureScore = player.score.treasure
-            let scored = [
-                player.score.temple,
-                player.score.farm,
-                player.score.settlement,
-                player.score.market
-            ]
-            for (let i = 0; i < treasureScore; i++) {
-                let minimumScore = Math.min(...scored)
-                let index = scored.indexOf(minimumScore)
-                scored.splice(index, 1)
-                scored.push(++minimumScore)
+        getWinnerName(playerId) {
+            const player = this.$store.getters['players/getPlayer'](playerId)
+            return player ? player.name : ''
+        },
+        getScores() {
+            let playerScores = []
+            for (const player of this.allPlayers) {
+                let treasureScore = player.score.treasure
+                let scored = [
+                    player.score.temple,
+                    player.score.farm,
+                    player.score.settlement,
+                    player.score.market
+                ]
+                for (let i = 0; i < treasureScore; i++) {
+                    let minimumScore = Math.min(...scored)
+                    let index = scored.indexOf(minimumScore)
+                    scored.splice(index, 1)
+                    scored.push(++minimumScore)
+                }
+                scored.sort((a,b) => a - b)
+                playerScores.push({
+                    player: { ...player },
+                    score:[...scored]
+                })
             }
-            playerScores.push({
-                player: { ...player },
-                finalScore: Math.min(...scored)
+            // sort taking into account tie breaker
+            playerScores.sort((a, b) => {
+                let firstScore = b.score[0] - a.score[0]
+                if (firstScore) return firstScore
+                let secondScore = b.score[1] - a.score[1]
+                if (secondScore) return secondScore
+                let thirdScore = b.score[2] - a.score[2]
+                if (thirdScore) return thirdScore
+                let fourthScore = b.score[3] - a.score[3]
+                if (fourthScore) return fourthScore
             })
+            return playerScores
         }
-        playerScores.sort((a, b) => b.finalScore - a.finalScore)
-        return playerScores
-      }
     }
 }
 </script>
 
 <style lang="scss" scoped>
 [class|="confetti"] {
-  position: absolute;
+    position: absolute;
 }
 
 $colors: (#d13447, #ffbf00, #263672);
 
 @for $i from 0 through 150 {
-  $w: random(8);
-  $l: random(100);
-  .confetti-#{$i} {
-    width: #{$w}px;
-    height: #{$w*0.4}px;
-    background-color: nth($colors, random(3));
-    top: -10%;
-    left: unquote($l+"%");
-    opacity: random() + 0.5;
-    transform: rotate(#{random()*360}deg);
-    animation: drop-#{$i} unquote(1+random()+"s") unquote(random()+"s") infinite;
-  }
-
-  @keyframes drop-#{$i} {
-    100% {
-      top: 110%;
-      left: unquote($l+random(5)+"%");
+    $w: random(8);
+    $l: random(100);
+    .confetti-#{$i} {
+        width: #{$w}px;
+        height: #{$w*0.4}px;
+        background-color: nth($colors, random(3));
+        top: -10%;
+        left: unquote($l+"%");
+        opacity: random() + 0.5;
+        transform: rotate(#{random()*360}deg);
+        animation: drop-#{$i} unquote(1+random()+"s") unquote(random()+"s") infinite;
     }
-  }
+
+    @keyframes drop-#{$i} {
+        100% {
+            top: 110%;
+            left: unquote($l+random(5)+"%");
+        }
+    }
 }
 </style>
